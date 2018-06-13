642d5770fea1bcb43b45f3763aec36149c70e466
tetsu <tetete1118@gmail.com>
2018-06-14 00:14:29 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,49 +1,35 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="hoge">bar</label>
-//         <input type="radio" id="hoge" onChange={() => { console.log("hoge") }} />
-//         <label htmlFor="hoge2">bar</label>
-//         <input type="radio" id="hoge2" onChange={() => { console.log("hoge") }} />
-//         <label htmlFor="hoge3">bar</label>
-//         <input type="text" id="hoge3" onChange={() => { console.log("hoge") }} />
-//       </React.Fragment>
-//     );
-//   }
-// }
+const App = () => {return <Counter></Counter>}
 
-const App = () => {
-  const users = [
-    {name: 1},
-    {name: "hoge",age: 10},
-    {name: "hogehgoe",age: 100},
-    {name: "hogehgoehoge",age: 1000}
-  ]
-  return(
-    <div>
-      {
-        users.map((user, index) => {
-          return <User name={user.name} age={user.age} key={index} />
-        })
-      }
-    </div>
-  )
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+  Increment = () => {
+    var increment_count = this.state.count + 1
+    this.setState({
+      count: increment_count
+    })
+  }
+  Decrement = () => {
+    var decrement_count = this.state.count - 1
+    this.setState({
+      count: decrement_count
+    })
+  }
+  render(){
+    return(
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.Increment}>+</button>
+        <button onClick={this.Decrement}>-</button>
+      </React.Fragment>
+    )
+  }
 }
-
-const User = (props) => {
-  return(
-    <div>hi i am {props.name}! {props.age} years old!</div>
-  )
-}
-
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
-
 
 export default App;
