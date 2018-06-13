77ef5a7064a33b4a7bdb22665a2fd9f555cc696c
tetsu <tetete1118@gmail.com>
2018-06-14 00:33:56 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+// reducer側でも活用するため定数として定義してexportして置く
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => {
+  type: INCREMENT
+}
+
+export const increment = () => {
+  type: DECREMENT
+}
