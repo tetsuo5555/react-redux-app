a27c5bfdc106f9e5a54934d03ec161ed15232c12
tetsu5555 <tetete1118@gmail.com>
2018-06-18 22:01:31 +0900

    apply material-ui to events-new and events-show components

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import MuiThemeProvider form 'material-ui/styles/MuiThemeProvider'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import './index.css'
 import reducer from './reducers'
